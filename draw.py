7d4ba89963f2d22bf4f443810324118fdc59309b
John Lin <jlin22@stuy.edu>
2018-02-11 15:55:11 -0500

    finished

--- draw.py
+++ draw.py
@@ -8,41 +8,96 @@ def draw_line( x0, y0, x1, y1, screen, color ):
 	dx = x1-x0
 	A = dy
 	B = -dx
+	x = x0
+	y = y0
+
 	# 0 < dy/dx < 1 
 	# Octant 1
-	if 0 < dy < dx : 
-		x=x0, y=y0
-		d=2*A+B
-		while x <= x1:
-    		plot(screen, color, x, y)	
-    		if d>0:
-   	    		y++
-   	    		d+=2*B
-   			x++
-   			d+=2*A
+	if (0 < dy < dx): 
+		d= 2*A+B
+		while (x <= x1):
+			plot(screen, color, x, y)
+			if (d>0):
+				y+=1	
+				d+=2*B
+			x+=1
+			d+=2*A
 
-	
+
 	# 1 < dy/dx 
 	# Octant 2
-	elif dx < dy :
+	elif (dx < dy):
+		d = A+2*B
+		while (y <= y1):
+			plot(screen, color, x, y)
+			if (d<0):
+				x+=1	
+				d+=2*A
+			y+=1
+			d+=2*B
 
-	}
+
+
 	# -1 < dy/dx < 0
 	# Octant 8
-	elif -dx < dy < 0:
+	elif (-dx < dy < 0):
+		d = 2*A-B
+		while (x <= x1):
+			plot(screen, color, x, y)
+			if (d<0):
+				y-=1
+				d-=2*B
+			x+=1
+			d+=2*A
+
+
 
 	# dy/dx < -1
 	# Octant 7
-	elif dy < -dx:	
+	elif (dy < -dx):	
+		d = A-2*B
+		while (y >= y1):
+			plot(screen, color, x, y)
+			if (d>0):
+				x+=1
+				d+=2*A
+			y-=1
+			d-=2*B
 
 	# dy/dx = 1
-	elif dy = dx:	
+	elif (dy == dx):	
+		while (x <= x1):
+			plot(screen, color, x, y)
+			x+=1
+			y+=1
+
+
 	# dy/dx = -1
-	elif dy = -dx :
+	elif (dy == -dx):
+		while (x <= x1):
+			plot(screen,color, x,y)
+			x+=1
+			y-=1
+
 	# dy/dx = 0
-	elif dy = 0 :
+	elif (dy == 0) :
+		while (x<=x1):
+			plot(screen, color, x,y)
+			x+=1
+
+
 	# dy/dx = infinity
-	elif dx = 0 and dy > 0:
+	elif (dx== 0 and dy > 0):
+		while (y<=y1):
+			plot(screen,color,x,y)
+			y+=1
+
+
 	# dy/dx = -infinity	
-	elif dx = 0 and dy < 0:
-    pass
+	elif (dx == 0 and dy < 0):
+		while (y>=y1):
+			plot(screen,color,x,y)
+			y-=1
+
+
+	pass
--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from display import *
 from draw import *
 
+#500 x500
 screen = new_screen()
-color = [ 0, 255, 0 ]
 
-#display(screen)
-#save_extension(screen, 'img.png')
+color1 = [0, 0, 255]
+draw_line(0,0, 250, 500, screen, color1)
+color2 = [0, 255, 255]
+draw_line(0,0, 500, 250, screen, color2)
+color3 = [120,250,90]
+draw_line(0,500, 500, 250, screen, color3)
+color4 = [0, 255, 0]
+draw_line(0,500, 250, 0, screen, color4)
+color5 = [0,255,0]
+draw_line(0,0,500,500, screen, color5)
+color6 = [255,0,0]
+draw_line(0,500,500,0, screen, color6)
+color7 = [255,0,0]
+draw_line(0,250,500,250, screen,color7)
+color8= [0,0,255]
+draw_line(0,0,0,500, screen, color8)
+color9= [255,0	,120]
+draw_line(499, 499, 499,0, screen, color9)
+display(screen)
+save_extension(screen, 'img.ppm')
